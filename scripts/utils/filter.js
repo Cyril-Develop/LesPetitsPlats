56e9581bf71f57f1e4d523b70d4548db5767567d
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2023-09-13 10:20:40 +0200

    responsive dropdown section + modif logique btn drop

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -1,9 +1,23 @@
 export const openCloseFilter = () => {
-    const btnDrop = document.querySelectorAll('.dropdown_btn')
+    const dropdownButtons = document.querySelectorAll('.dropdown_btn');
 
-    btnDrop.forEach(btn => {
-        btn.addEventListener('click', () => {
-            btn.nextElementSibling.classList.toggle('active')
-        })
-    })
-}
+    function toggleDropdown(button) {
+        const dropdown = button.nextElementSibling;
+        dropdown.classList.toggle('active');
+    };
+
+    function closeOtherDropdowns(clickedButton) {
+        dropdownButtons.forEach(button => {
+            if (button !== clickedButton) {
+                button.nextElementSibling.classList.remove('active');
+            }
+        });
+    };
+
+    dropdownButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            toggleDropdown(button);
+            closeOtherDropdowns(button);
+        });
+    });
+};
